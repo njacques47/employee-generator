9951c614e0ee705d3a961b0936eb5df2f7cbb883
njacques47 <nojacques94@gmail.com>
2022-03-13 23:54:57 -0400

    created an empty array to push objects to and created a fn to evaluate objects pushed

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+// push created employees here??
+const teamArray = [];
 
 // made this into a function to loop through if necessary (previously just an array of Qs)
 function onboardingQs() {
@@ -31,27 +33,27 @@ function onboardingQs() {
   },
   {
     type: 'list',
-    name: 'role.assignment',
+    name: 'role',
     message: "What is this person's role?",
     choices: ['Manager', 'Engineer', 'Intern']
   },
   {
     type: 'number',
-    name: 'role.manager',
+    name: 'officeNumber',
     message: 'What is their office number? (numerical values only)',
-    when: answer.role.assignment === "Manager"
+    when: answer.role === "Manager"
   },
   {
     type: 'input',
-    name: 'role.engineer',
+    name: 'github',
     message: 'What is their github username?',
-    when: answer.role.assignment === "Engineer"
+    when: answer.role === "Engineer"
   },
   {
     type: 'input',
-    name: 'role.intern',
+    name: 'school',
     message: 'What school does this intern attend?',
-    when: answer.role.assignment === "Intern"
+    when: answer.role === "Intern"
   },
   {
     type: 'confirm',
@@ -59,6 +61,22 @@ function onboardingQs() {
     message: 'Would you like to add another employee to the team?'
   }
   ])
+}
+
+// create new employees based on satisfied conditions
+function employeeProfile() {
+  // if manager, return new manager obj and push it to array
+  if (teamData.role === 'Manager'){
+    //
+  }
+  // if engineer, return new engineer obj and push it to array
+  else if (teamData.role === 'Engineer'){
+    //
+  }
+  // if intern, return new intern obj and push it to array
+  else if (teamData.role === 'Intern'){
+    //
+  }
 }
 
 // runs all the important things to make the app work
@@ -70,16 +88,16 @@ function buildTeam() {
   `);
 
   // prompt Qs
-  inquirer
-    .prompt(onboardingQs)
-    .then((teamData) => {
-      // if they would like to add another employee, 
-      if (teamData.addEmployee === true) {
-        return inquirer.prompt(onboardingQs(teamData));
-      }
+  onboardingQs()
+  .then((teamData) => {
+    // if they would like to add another employee, add a new employee
+    if (teamData.addEmployee === true) {
+      return onboardingQs(teamData);
+    }
 
+    //
 
-    })
+  })
 }
 
 
@@ -96,10 +114,6 @@ function buildTeam() {
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
